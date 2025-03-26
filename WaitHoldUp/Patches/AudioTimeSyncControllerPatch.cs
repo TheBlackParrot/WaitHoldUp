4e899671bcf676a9d83539473d9a9be9f930aa1d
TheBlackParrot <theblackparrot0@gmail.com>
2025-03-26 11:44:21 -0500

    Initial commit

---
+++ WaitHoldUp/Patches/AudioTimeSyncControllerPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace WaitHoldUp.Patches;
+
+[HarmonyPatch]
+internal class AudioTimeSyncControllerPatch
+{
+    private static PauseController? _pauseController;
+    
+    [HarmonyPatch(typeof(AudioTimeSyncController), "StartSong")]
+    // ReSharper disable once InconsistentNaming
+    internal static void Postfix(AudioTimeSyncController __instance)
+    {
+        if (_pauseController == null)
+        {
+            _pauseController = Object.FindObjectOfType<PauseController>();
+        }
+
+        _pauseController?.Pause();
+    }
+}
---
+++ WaitHoldUp/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using IPA;
+using JetBrains.Annotations;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace WaitHoldUp;
+
+[Plugin(RuntimeOptions.DynamicInit), NoEnableDisable]
+[UsedImplicitly]
+internal class Plugin
+{
+    private static Harmony _harmony = null!;
+    private static IPALogger Log { get; set; } = null!;
+
+    [Init]
+    public Plugin(IPALogger ipaLogger, Zenjector zenjector)
+    {
+        Log = ipaLogger;
+        zenjector.UseLogger(Log);
+        
+        Log.Info("Plugin loaded");
+    }
+
+    [OnEnable]
+    public void OnEnable()
+    {
+        _harmony = new Harmony("TheBlackParrot.WaitHoldUp");
+        _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        
+        Log.Info("Patches applied");
+    }
+    
+    [OnDisable]
+    public void OnDisable()
+    {
+        _harmony.UnpatchSelf();
+    }
+}
